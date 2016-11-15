6ac9d10a1ddc771663ec3602a4e2a5ea6abb3012
deborahak <deborahkain@mac.com>
2016-11-15 08:05:55 -0800

    add map

--- js/app.js
+++ js/app.js
@@ -1,29 +1,9 @@
-//showResults --returns top 10 museums
-/*var showSearchResults = function() {
-    var results =
-}*/
-$(document).ready(function() {
-    $(".city-search").submit(function(event) {
-        event.preventDefault();
-        $(".results").html("");
-        var tags = $(this).find("input[name='cityMuseums']").val();
-        getCity();
-    });
-    $(".country-search").submit(function(event) {
-        event.preventDefault();
-        $("results").html("");
-        var tag = $(this).find("input[name='countryMuseums'").val();
-        getCountry();
-    });
-});
-
-var getCity = function(tags) {
+var getCity = function() {
     var request = {
-        city: $("#city").val(), ///'new york, or berlin or any city on earth', // hint: use jQuery's .val() some how ;-)
-        country: "united-states", // for now.
-        //order:
+        city: $("#cities").val(),
+        country: $("#country").val().toLowerCase()
     };
-
+    // console.log(request);
     $.ajax({
             url: "http://www.muselia.com/apimuseums",
             data: request,
@@ -31,24 +11,77 @@ var getCity = function(tags) {
             type: "GET",
         })
         .done(function(result) {
-            console.log(result);
-
-        });
-
-}
-
-var getCountry = function(tag) {
-    var request = {
-        country: $("#country").val(),
-    };
-    $.ajax({
-        url: "http://www.muselia.com/apimuseums",
-        data: request,
-        dataType: "JSONP",
-        type: "GET",
-    })
-    .done(function(result) {
-            console.log(result);
-
+            showSearchResults(result);
+            // finished//
         });
 };
+
+var showSearchResults = function(museums) {
+    // what is museum? some obj
+    var result = $(".templates-hidden").clone();
+    var html = "";
+    var results = [];
+    for (var i = 0; i < museums.length; i++) {
+        console.log(museums[i]);
+        var museumElem = result.find(".museum_name a");
+        html += "<li><a href=''>" + museums[i].name + "</a>"+ museums[i].address + "<br/>" + "<img src='" + museums[i].image + "'></li>";
+        // results.push(museumElem);
+        // console.log(museumElem);
+    }
+    // console.log(results);
+    $(".results").append(html);
+    // hints: Museums is an array
+    // when we have arrays, to work with them the best way is usually
+    // by looping. We can loop using "for" loops
+
+    // var result = $(".templates-hidden").clone();
+    // var museumElem = result.find(".museum_name a");
+    // museumElem.text(?.name);
+    // museumElem.attr()
+
+};
+
+function initMap(){
+    var map = new google.maps.Map(document.getElementById("museum_map"), {
+        zoom: 4
+    });
+    var marker = new google.maps.Marker({
+        marker_properties ,
+      //  position: ,
+        map: map
+    });
+};
+
+var countryCity = {
+    spain: ["Madrid", "Barcelona"],
+    france: ["Paris", "Lyon", "Marseille"],
+    ireland: ["Dublin", "Cork"]
+};
+
+$(document).ready(function() {
+    $(".museumSearch").click(function(event) {
+        event.preventDefault();
+        $(".results").html("");
+        getCity();
+    });
+
+    // handles our dropdowns
+    $("#country").on('input', function() {
+
+        var country = this.value.toLowerCase();
+        //console.log(country);//
+        /// start here.
+        var cities = countryCity[country]; // spain
+        //console.log(cities);
+        var html = "";
+        for (var i = 0; i < cities.length; i++) {
+            // cities[0] ; or i <<-----
+            html += '<option value="' + cities[i].toLowerCase() + '">' + cities[i] + '</option>';        }
+
+        $("#cities").empty();
+        // #citis is empty
+        $("#cities").append(html);
+
+    });
+
+});
