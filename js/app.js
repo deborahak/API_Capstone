c95b0677f242aef0ef5a826765e870d37d2eac9e
deborahak <deborahkain@mac.com>
2016-11-29 23:15:00 -0800

    list and map

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var getCity = function() {
     var request = {
         city: $("#cities").val(),
-        country: $("#country").val().toLowerCase()
+        country: $("#country").val().toLowerCase(),
+        coordinates: 'coordinates',
+        url: 'url'
     };
     // console.log(request);
     $.ajax({
@@ -13,8 +15,10 @@ var getCity = function() {
         .done(function(result) {
             showSearchResults(result);
             // finished//
+            initMap(result);
         });
 };
+
 
 var showSearchResults = function(museums) {
     // what is museum? some obj
@@ -22,9 +26,9 @@ var showSearchResults = function(museums) {
     var html = "";
     var results = [];
     for (var i = 0; i < museums.length; i++) {
-        console.log(museums[i]);
+        //console.log(museums[i]);
         var museumElem = result.find(".museum_name a");
-        html += "<li><a href=''>" + museums[i].name + "</a>"+ museums[i].address + "<br/>" + "<img src='" + museums[i].image + "'></li>";
+        html += "<li><a href='museums[i].url '>" + museums[i].name + "</a>" + "<br/>" + museums[i].address + "<br/>" + "<img src='" + museums[i].image + "'></li>";
         // results.push(museumElem);
         // console.log(museumElem);
     }
@@ -41,16 +45,65 @@ var showSearchResults = function(museums) {
 
 };
 
-function initMap(){
+var infowindow;
+
+function initMap(locations) {
+    // this is what happens when the page is loaded
+    // extracting coordinates from the API
+    //grab lat and long from coordinates, 
+    //split into location using parseFloat
+
+    var location = locations[0];
+    var coords = location["coordinates"].split(",");
+    var uluru = {
+        lat: parseFloat(coords[0]),
+        lng: parseFloat(coords[1])
+    };
+    //console.log(uluru);
     var map = new google.maps.Map(document.getElementById("museum_map"), {
-        zoom: 4
+        zoom: 10,
+        center: uluru
     });
-    var marker = new google.maps.Marker({
-        marker_properties ,
-      //  position: ,
-        map: map
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //console.log(locations);
+
+    var markers = locations.map(function(location, i) {
+
+        var coords = location["coordinates"].split(",");
+        var marker = new google.maps.Marker({
+            position: {
+                lat: parseFloat(coords[0]),
+                lng: parseFloat(coords[1])
+            },
+            label: labels[i % labels.length],
+        });
+        var contentString = "<div id='content'>" +
+            "<div id='siteNotice'>" + "</div>" +
+            "<h1 id='firstHeading' class='firstHeading'>" + location.name + "</h1>" +
+            "<div id='bodyContent'>" + location.address + "</div>";
+
+        infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+        });
+        marker.addListener('click', function() {
+            if (infowindow) {
+                infowindow.close();
+            }
+            infowindow.open(map, marker);
+        });
+
+        return marker;
+
     });
+    //    markers.addListener("click", function(){
+    //        infowindow.open(map, marker);
+    //    });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+
 };
+
 
 var countryCity = {
     spain: ["Madrid", "Barcelona"],
@@ -64,7 +117,6 @@ $(document).ready(function() {
         $(".results").html("");
         getCity();
     });
-
     // handles our dropdowns
     $("#country").on('input', function() {
 
@@ -76,10 +128,10 @@ $(document).ready(function() {
         var html = "";
         for (var i = 0; i < cities.length; i++) {
             // cities[0] ; or i <<-----
-            html += '<option value="' + cities[i].toLowerCase() + '">' + cities[i] + '</option>';        }
+            html += '<option value="' + cities[i].toLowerCase() + '">' + cities[i] + '</option>';
+        }
 
         $("#cities").empty();
-        // #citis is empty
         $("#cities").append(html);
 
     });
